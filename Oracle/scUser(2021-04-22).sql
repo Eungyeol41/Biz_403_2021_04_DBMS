aa5c78d41abaeb114944b9c465444919ad41e1f5
Eungyeol41 <iniziato@naver.com>
2021-04-22 10:50:44 +0900

    Oracle

---
+++ Oracle/scUser(2021-04-22).sql
@@ -0,0 +1,106 @@
+-- 여기는 scUser로 접속
+CREATE TABLE tbl_student (
+        st_num CHAR(5),
+        st_name nVARCHAR2(20),
+        st_dept nVARCHAR2(10), 
+        st_grade VARCHAR(5),
+        st_tel VARCHAR(20), 
+        st_addr nVARCHAR2(125)
+);
+
+DROP TABLE tbl_student;
+
+-- 생성한 Table에 데이터 추가
+-- DML (데이터 조작어) 명령을 사용하여 데이터 추가(create)
+-- create : '테이블에 존재하지 않는 데이터를 새로 추가한다'라는 개념
+INSERT INTO tbl_student(st_num, st_name, st_dept, st_grade)
+VALUES ('00001', '홍길동', '국어국문', '3');
+
+-- 데이터를 추가한 후에는 잘 추가되었는 지 확인
+-- tbl_student table에 저장되어 있는 모든 데이터를 무조건 보여달라
+SELECT * FROM tbl_student;
+
+INSERT INTO tbl_student(st_num, st_dept, st_grade)
+VALUES ('00001', '컴퓨터공학', '2');
+SELECT * FROM tbl_student;
+
+-- 위에서 생성한 tbl_student 테이블에는 데이터를 추가하려고 할 때
+-- 이름 데이터가 없어도 데이터가 정상적으로 추가가 되어버린다
+-- 같은 학번의 데이터가 이미 추가되어 있어도 또 다시 추가가 되어버린다
+-- 이런 식으로 데이터가 계속 추가된다면 전체 데이터의 신뢰성에 문제가 될 것이다
+-- DBMS에서는 table(Entity)를 설계할 때 이러한 오류를 방지하기 위하여 Table을 생성할 때 '제약 조건'을 설정하여 데이터가 INSERT되지 못하도록 하는 기능이 있다.
+-- 작성된 table을 삭제하고 다시 제약 조건을 설정하여 생성하자
+
+DROP TABLE tbl_student CASCADE CONSTRAINTS;
+
+-- 1. 학생의 이름을 데이터가 반드시 있어야만 한다
+--      st_name(학생 이름) 칼럼은 NOT NULL이어야 한다
+-- 2. 학번은 절대 중복되면 안 된다
+--      tbl_student 테이블의 모든 데이터의 학번은 유일해야 한다
+CREATE TABLE tbl_student (
+        st_num CHAR(5) UNIQUE NOT NULL,
+        st_name nVARCHAR2(20) NOT NULL,
+        st_dept nVARCHAR2(10), 
+        st_grade VARCHAR2(5), -- 숫자값을 입력, 숫자값을 문자형으로 인식
+        st_tel VARCHAR2(20),  -- 000.0000.0000
+        st_addr nVARCHAR2(125)
+);
+
+-- 학생이름 데이터가 없어서 INSERT 불가
+INSERT INTO tbl_student (st_num, st_dept)
+VALUES ('00001', '컴퓨터공학');
+
+-- 학생이름 데이터를 같이 포함하여 INSERT 수행
+INSERT INTO tbl_student (st_num, st_dept, st_name)
+VALUES ('00001', '컴퓨터공학', '홍길동');
+
+-- 학번 칼럼이 NOT NULL되어 있어서 INSERT 불가
+INSERT INTO tbl_student (st_dept, st_name)
+VALUES ('사회과학', '이몽룡');
+
+-- st_num 칼럼이 UNIQUE인데 이미 존재하는 00001 학번으로 데이터를 추가하려고 하니 문제가 있어서 INSERT 불가
+-- Table의 제약조건을 설정할 때 UNIQUE는 매우 신중하게 선택해야 한다
+INSERT INTO tbl_student (st_num, st_dept, st_name)
+VALUES ('00001', '사회과학', '이몽룡');
+
+INSERT INTO tbl_student (st_num, st_dept, st_name)
+VALUES ('00010', '사회과학', '이몽룡');
+
+INSERT INTO tbl_student (st_num, st_dept, st_name)
+VALUES ('00002', '법학과', '성춘향');
+SELECT * FROM tbl_student;
+
+-- 기본키 칼럼 (primary key)
+-- 데이터를 조회 (SELECT)할 때 st_num 칼럼을 기준으로 조회를 하면 반드시 원하는 데이터 1개만 보여지는 조건을 만족하게 하는 칼럼
+-- 제약조건이 반드시 UNIQUE하면서 NOT NULL이어야 한다
+-- 기본키는 제약 조건에 UNIQUE와 NOT NULL을 같이 설정해야 하는데, DBMS에서는 기본키 제약 조건을 설정하는 키워드가 별도로 있다
+
+-- PRIMARY KEY : UNIQUE + NOT NULL + 기타 조건 + INDEX 자동 생성
+--      매우 강력한, 가장 우선순위가 높은 제약조건이다.
+CREATE TABLE tbl_student (
+        st_num CHAR(5) PRIMARY KEY,
+        st_name nVARCHAR2(20) NOT NULL,
+        st_dept nVARCHAR2(10), 
+        st_grade VARCHAR2(5), -- 숫자값을 입력, 숫자값을 문자형으로 인식
+        st_tel VARCHAR2(20),  -- 000.0000.0000
+        st_addr nVARCHAR2(125)
+);
+-- TABLE의 구조를 보여달라
+DESCRIBE tbl_student;
+DESC tbl_student;
+
+INSERT INTO tbl_student (st_num, st_dept, st_name)
+VALUES ('00001', '사회과학', '이몽룡');
+
+INSERT INTO tbl_student (st_num, st_dept, st_name)
+VALUES ('00100', '법학과', '성춘향');
+
+INSERT INTO tbl_student (st_num, st_dept, st_name)
+VALUES ('00002', '컴퓨터공학', '홍길동');
+SELECT * FROM tbl_student;
+
+-- PK로 설정된 칼럼에 조건을 부여하여 데이터 조회하기
+SELECT
+    *
+FROM tbl_student
+WHERE st_num = '00001';
---
+++ Oracle/관리자(2021-04-22).sql
@@ -0,0 +1,24 @@
+-- 여기는 관리자 권한으로 접속
+
+DROP TABLESPACE schoolDB
+INCLUDING CONTENTS AND DATAFILES
+CASCADE CONSTRAINTS;
+
+CREATE TABLESPACE schoolDB
+DATAFILE 'C:\oraclexe\data/school.dbf'
+SIZE 1M AUTOEXTEND ON NEXT 1K;
+
+-- Schema : table, index, view 등등 데이터의 전체적인 모음집
+-- 객체들의 정보를 담는 곳
+-- 데이터들의 모음체
+-- 표준 SQL : CREATE SCHEMA
+CREATE USER scUser IDENTIFIED BY scUser
+DEFAULT TABLESPACE schoolDB;
+
+-- 권한 부여, 실습을 위해서 DBA 권한을 부여하자
+-- DBA 권한을 남발하면 DB 보안적인 측면에서 무결성을 해칠 수 있는 여지가 많아진다
+-- DB와 관련된 보안 용어
+--  보안 침해 : 허갑다지 않은 사용자가 접속하여 문제를 일으키는 것
+--  무결성 침해(파괴) : 허가받은 사용자가 권한을 남용하여 문제 일으키는 것
+--          CUD(추가, 수정, 삭제) 등을 잘못하여 데이터에 문제가 생기는 것
+GRANT DBA TO scUser;
