5976f9d12bc2f45a8a9c8dda216a536e6031b31d
Eungyeol41 <iniziato@naver.com>
2021-05-06 13:06:46 +0900

    이름 수정

--- 하나마트/관리자(2021-05-06-하나마트).sql
+++ 하나마트/관리자(2021-05-06-하나마트).sql
@@ -1,6 +1,10 @@
 -- 관리자 접속
-DROP USER iouser CASCADE;
 
+-- 시스템에 작성된 TableSpace를 확인하기
+SELECT * FROM DBA_TABLESPACES;
+
+-- 사용자 지우고 TableSpace 지우기
+DROP USER iouser CASCADE;
 DROP TABLESPACE iolistDB
 INCLUDING CONTENTS AND DATAFILES
 CASCADE CONSTRAINTS;
@@ -13,3 +17,15 @@ CREATE USER iouser IDENTIFIED BY iouser
 DEFAULT TABLESPACE iolistDB;
 
 GRANT DBA TO iouser;
+
+-- 오라클의 System 값
+SELECT * FROM DBA_PROFILES;
+
+-- 오라클에 새로 등록된 사용자의 Password 만료일자
+-- 기본값이 180으로 되어있어서 새로운 사용자 등록 후 비번을 변경하지 않으면 180일 후에는 접속이 불가능해진다.
+SELECT * FROM DBA_PROFILES
+WHERE resource_name = 'PASSWORD_LIFE_TIME';
+
+ALTER PROFILE 
+DEFAULT LIMIT PASSWORD_LIFE_TIME
+UNLIMITED;
